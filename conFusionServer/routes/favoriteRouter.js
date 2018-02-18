d870911ecab3e0239e37f804f1d9e1bae48619a1
changx03 <luke.x.chang@gmail.com>
2018-02-18 17:28:59 +1300

    completed task 2 and 3

--- conFusionServer/routes/favoriteRouter.js
+++ conFusionServer/routes/favoriteRouter.js
@@ -82,17 +82,118 @@ favoriteRouter
     res.end('PUT operation is not supported on /favorites');
   });
 
+favoriteRouter
+  .route('/:dishID')
+  .options(cors.corsWithOptions, (req, res, next) => {
+    res.sendStatus(200);
+  }) // enable pre-flight request
+  .get(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    // There is no insturction for GET '/favorites/:dishId'
+    res.statusCode = 403; // not supported
+    res.end(
+      `Get operation is not supported on /favorites/${req.params.dishID}`
+    );
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+      .then(favorite => {
+        if (favorite) {
+          saveDish(favorite, req.params.dishID, (err, favorite) => {
+            if (err) {
+              next(err);
+            } else {
+              jsonResponse200(res, favorite);
+            }
+          });
+        } else {
+          Favorite.create({
+            user: req.user._id,
+          })
+            .then(favorite => {
+              saveDishes(favorite, req.params.dishID, (err, favorite) => {
+                if (err) {
+                  next(err);
+                } else {
+                  jsonResponse200(res, favorite);
+                }
+              });
+            })
+            .catch(err => next(err));
+        }
+      })
+      .catch(err => next(err));
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+      .then(favorite => {
+        if (!favorite) {
+          let err = new Error(
+            `Cannot remove ${
+              req.params.dishID
+            }. You don't have any favorite yet.`
+          );
+          return next(err);
+        } else {
+          // if dishID is not in favorite, this will return favorite untouched
+          favorite.dishes.pull({ _id: req.params.dishID });
+          favorite
+            .save()
+            .then(favorite => {
+              jsonResponse200(res, favorite);
+            })
+            .catch(err => next(err));
+        }
+      })
+      .catch(err => next(err));
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403; // not supported
+    res.end(
+      `PUT operation is not supported on /favorites/${req.params.dishID}`
+    );
+  });
+
+/**
+ * Helper function for success response
+ *
+ * @param {*} res
+ * @param {*} body
+ */
 function jsonResponse200(res, body) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json(body);
 }
 
+/**
+ * Add unique dishes to favorite
+ *
+ * @param {object} fav
+ * @param {[string]} dishes
+ * @param {function} callback
+ */
 function saveDishes(fav, dishes, callback) {
   dishes.forEach(dish => {
-    // ensure we dont want to see duplicated dishID
+    // ensure we dont want to see duplicated dishId
     fav.dishes.addToSet(dish._id);
   });
+  fav
+    .save()
+    .then(favorite => {
+      callback(null, favorite);
+    })
+    .catch(err => callback(err, favorite));
+}
+
+/**
+ * Add one unique dish to favorite
+ *
+ * @param {object} fav
+ * @param {string} dishID
+ * @param {function} callback
+ */
+function saveDishes(fav, dishID, callback) {
+  fav.dishes.addToSet(dishID);
   fav
     .save()
     .then(favorite => {
